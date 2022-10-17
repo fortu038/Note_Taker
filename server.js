36bf75b940027d568f09586ed5ec848bb6010dc8
fortu038 <fortu038@umn.edu>
2022-10-17 12:45:07 -0500

    added basic structure to server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const db = require("./db/db.json");
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`);
+});
+
